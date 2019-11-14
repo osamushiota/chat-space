4057086c2bfa69f3512753c3e2119ad3bcea0581
osamushiota <os036@pa2.so-net.ne.jp>
2019-11-14 21:31:37 +0900

    update02

---
+++ app/assets/javascripts/KEEP.js
@@ -0,0 +1,41 @@
+$(function(){
+ function buildHTML(message){
+   let image = <img class="lower-message__image" src="${message.image}" alt="Img 2020" />
+   let html = `<div class=‘message’ data-id=“${message.id}“>
+   <div class=‘upper-message’>
+   <div class=‘upper-message__user-name’>
+   ${message.name}
+   </div>
+   <div class=‘upper-message__date’>
+   ${message.date}
+   </div>
+   </div>
+   <div class=‘lower-message’>
+   <p class=‘lower-message__content’>
+   ${message.content}
+   </p>
+   ${image}
+   </div>
+   </div>`
+   return html;
+ };
+ 
+ $(‘.new_message’).on(‘submit’, function(e){
+   let formData = new FormData(this);
+   let url = $(this).attr(‘action’);
+   $.ajax({
+     url: url,
+     type: ‘POST’,
+     data: formData,
+     dataType: ‘json’,
+     processData: false,
+     contentType: false,
+  })
+   .done(function(data){
+     let html = buildHTML(data)
+     $(‘.messages’).append(html);
+     $(‘.messages’).animate({scrollTop: $(‘.messages’)[0].scrollHeight}, ‘fast’);
+     $(‘.submit-btn’).attr(‘disabled’, false);
+   })
+   .fail(function(){
+     alert(‘error’);
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,10 @@
+# json.groups  @group.group
+# json.message  @messages.message
+# json.user_id  @current.user.id
+# json.user_name  @current.user.nickname
+# json.group_users  @current.user.id
+# json.image @message.image
+json.name  @message.user.name
+json.group @group_users
+json.group @group
+
