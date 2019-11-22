f65da578dc03367a54755d58172acc2162851c54
osamushiota <os036@pa2.so-net.ne.jp>
2019-11-22 14:55:57 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,31 +50,31 @@ $(function(){
     });
   })
 
-  // let reloadMessages = function () {
-  //   if (window.location.href.match(/\/groups\/\d+\/messages/)){      
-  //     let last_message_id = $('.message:last').data("message-id");   
+  let reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){      
+      let last_message_id = $('.message:last').data("message-id");   
 
-  //     $.ajax({
-  //       url: "api/messages", 
-  //       type: 'get',
-  //       dataType: 'json',
-  //       data: {last_id: last_message_id} 
-  //     })
+      $.ajax({
+        url: "api/messages", 
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id} 
+      })
 
-  //     .done(function (messages) { 
-  //       let insertHTML = '';
-  //       messages.forEach(function (message) {
-  //         insertHTML = buildHTML(message); 
-  //         $('.messages').append(insertHTML);
-  //       })
-  //       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-  //     })
-  //     .fail(function () {
-  //       alert('自動更新に失敗しました');
-  //     });
-  //   }
-  // };
-  // setInterval(reloadMessages, 7000);
+      .done(function (messages) { 
+        let insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
 
 
